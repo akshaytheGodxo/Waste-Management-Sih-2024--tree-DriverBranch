762bb4c67375d121ea958ffdf9d928b3e0db29c1
Amadeus System <amadeussystem04@gmail.com>
2024-08-30 15:23:48 +0530

    in

---
+++ .history/drivers-interface/src/components/HomePage_20240830140529.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './HomePage/NavBar'
+const HomePage = () => {
+    return (
+    <div>
+        <NavBar />  
+
+        <h1>Landing Page in development</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ .history/drivers-interface/src/components/HomePage_20240830141127.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './HomePage/NavBar'
+import Hero from './HomePage/Hero'
+const HomePage = () => {
+    return (
+    <div>
+        <NavBar />  
+        <Hero/>
+
+        <h1>Landing Page in development</h1>
+    </div>
+  )
+}
+
+export default HomePage
